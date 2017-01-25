13438b6c2aef84a1cdbb3c848d8c59683e986e4b
OkadaGenya <g.okada.wilf+github@gmail.com>
2017-01-25 18:49:13 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,6 @@ import styles from './App.css';
 const App = () => (
   <div className={styles.app}>
     <h2>Hello, </h2>
-    <h2>Hello, Genya</h2>
-    <h2>Hello, </h2>
-    <h2>Hello, </h2>
-    <h2>Hello, </h2>
-    <h2>Hello, </h2>
   </div>
 );
 
